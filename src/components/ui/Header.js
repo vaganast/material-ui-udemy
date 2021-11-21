9bd9cb620b5ae2c5b7213608147da25bb00eefae
vaganast <71605687+vaganast@users.noreply.github.com>
2021-11-21 19:43:24 +0200

    style in line
    fix issue appbar cover hello

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -3,6 +3,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from '@material-ui/styles'
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -16,9 +17,16 @@ function ElevationScroll(props) {
     elevation: trigger ? 4 : 0,
   });
 }
+const useStyles = makeStyles(theme => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar
+  }
+}))
 
 export default function Header(props) {
+  const classes = useStyles()
   return (
+    <React.Fragment>
     <ElevationScroll>
       <AppBar position="fixed" color="primary">
         <Toolbar>
@@ -26,5 +34,7 @@ export default function Header(props) {
         </Toolbar>
       </AppBar>
     </ElevationScroll>
+    <div className= {classes.toolbarMargin} />
+    </React.Fragment>
   );
 }
