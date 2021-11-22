bb08b670d4889b5237f3a894ad2581a64ca9ded3
vaganast <71605687+vaganast@users.noreply.github.com>
2021-11-22 22:14:33 +0200

    menu and menu items add
    add menu and items at services tab with onMouseOver and onMouseLeave

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import theme from './ui/Theme';
-
 import Header from './ui/Header';
 
 function App() {
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -7,6 +7,8 @@ import { Tabs } from '@material-ui/core';
 import { Tab } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { Menu } from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
 
 import logo from '../../assets/logo.svg';
 
@@ -56,11 +58,22 @@ const useStyles = makeStyles(theme => ({
 
 export default function Header(props) {
   const classes = useStyles();
-
   const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
 
   const handleChange = (e, value) => {
     setValue(value);
+  };
+
+  const handleClick = e => {
+    setAnchorEl(e.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = e => {
+    setAnchorEl(null);
+    setOpen(false);
   };
 
   useEffect(() => {
@@ -107,8 +120,11 @@ export default function Header(props) {
               />
 
               <Tab
+                aria-owns={anchorEl ? 'simple-menu' : undefined}
+                aria-haspopup={anchorEl ? 'true' : undefined}
                 className={classes.tab}
                 component={Link}
+                onMouseOver={event => handleClick(event)}
                 to='/services'
                 label='Services'
               />
@@ -145,6 +161,19 @@ export default function Header(props) {
             >
               Free Estimate
             </Button>
+            <Menu
+              id='simple-menu'
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{ onMouseLeave: handleClose }}
+            >
+              <MenuItem onClick={handleClose}>
+                Custom Software Development
+              </MenuItem>
+              <MenuItem onClick={handleClose}>Mobile App Development </MenuItem>
+              <MenuItem onClick={handleClose}>Website Development </MenuItem>
+            </Menu>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
