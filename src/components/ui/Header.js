518f2f0e345a24124351dbf838f9f0d384f6afe3
vaganast <71605687+vaganast@users.noreply.github.com>
2021-11-22 22:26:48 +0200

    state update in menu items
    update the state in menu items and fix an issue hovering services tab wasnt clickable

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -169,16 +169,43 @@ export default function Header(props) {
               MenuListProps={{ onMouseLeave: handleClose }}
             >
               <MenuItem
-                onClick={handleClose}
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to='/services'
+              >
+                Services
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
                 component={Link}
                 to='/customsoftware'
               >
                 Custom Software Development
               </MenuItem>
-              <MenuItem onClick={handleClose} component={Link} to='/mobileapps'>
+              <MenuItem
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to='/mobileapps'
+              >
                 Mobile App Development{' '}
               </MenuItem>
-              <MenuItem onClick={handleClose} component={Link} to='/websites'>
+              <MenuItem
+                onClick={() => {
+                  handleClose();
+                  setValue(1);
+                }}
+                component={Link}
+                to='/websites'
+              >
                 Website Development{' '}
               </MenuItem>
             </Menu>
